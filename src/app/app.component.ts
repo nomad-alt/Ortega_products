356fda10e888f83de3e958902aa3a7a5db253e85
nurlanemir <nurlan.emiroff@gmail.com>
2017-10-24 12:33:23 +0200

    Creates new angular test project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  videos = [
+    {name: 'video one', liked: true},
+    {name: 'video two', liked: false}
+  ]
+  video = {
+    title: 'Drone footage',
+    views: 37000,
+    liked: true
+  }
+
+  handleClick(){
+    console.log('Someone clicked on button on the template')
+              }
 }
