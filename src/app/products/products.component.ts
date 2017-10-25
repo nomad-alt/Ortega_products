34762c7a7f0a6b8f329dc9bbdb3d955f1e8f8dda
nurlanemir <nurlan.emiroff@gmail.com>
2017-10-25 15:35:26 +0200

    Adds json file to retrieve data for products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IProduct} from './product';
 import {ProductService} from './product.service';
 
@@ -12,14 +12,17 @@ export class ProductsComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  errorMessage: string;
   _listFilter: string;
   get listFilter(): string {
     return this._listFilter;
   }
-  set listFilter(value:string) {
+
+  set listFilter(value: string) {
     this._listFilter = value;
     this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
   }
+
   filteredProducts: IProduct[];
   products: IProduct[];
 
@@ -41,8 +44,11 @@ export class ProductsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.products = this._productService.getProducts();
-    this.filteredProducts = this.products;
+    this._productService.getProducts()
+      .subscribe(products => {
+          this.products = products;
+          this.filteredProducts = this.products;
+        },
+        error => this.errorMessage = <any>error);
   }
-
 }
